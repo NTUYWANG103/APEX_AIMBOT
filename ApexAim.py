12be6d79c425ad10133be13bf5b97879d5d32f3e
wyx <wangyuxics@outlook.com>
2023-02-20 14:27:14 +0800

    max_lock_dis: 80

--- ApexAim.py
+++ ApexAim.py
@@ -231,5 +231,5 @@ if __name__ == '__main__':
     while True:
         apex.forward()
         if time.time() - heart_time > 600:
-            apex.login.loginHeart()
+            # apex.login.loginHeart()
             heart_time = time.time()
