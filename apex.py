557609dc1e67dd67d0e0dfd19e2996072fdcd9b7
wyx <wangyuxics@outlook.com>
2023-03-06 00:37:46 +0800

    apex 1280 default game settings

--- apex.py
+++ apex.py
@@ -7,6 +7,10 @@ class ApexAimBot(AimBot):
     def __init__(self, config_path, onnx_path, engine_path):
         super().__init__(config_path, onnx_path, engine_path)
 
+    def initialize_params(self):
+        super().initialize_params()
+        self.smooth = self.args.smooth * 1280 / self.args.resolution_x # default settings by game
+
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
